7209a30387a6e240765158cdaf4de706ee1338e7
Muhammadmustafa <mukhammadmustafa1888@gmail.com>
2026-05-16 16:42:35 +0500

    Add edit info action to admin menu scene

--- src/scenes/admin/adminMenu.scene.ts
+++ src/scenes/admin/adminMenu.scene.ts
@@ -13,6 +13,7 @@ adminMenuScene.enter(async (ctx) => {
     [Markup.button.callback('📅 Настроить расписание', 'add_schedule')],
     [Markup.button.callback('📶 Управление услугами', 'manage_service')],
     [Markup.button.callback('📋 Последние записи', 'view_appointments')],
+    [Markup.button.callback('📝 Изменить информацию о нас', 'edit_about_info')],
     [Markup.button.callback('⬅️ Выйти', 'exit_admin')]
   ]));
 });
@@ -21,6 +22,10 @@ adminMenuScene.action('manage_service', (ctx) => ctx.scene.enter('manage_service
 adminMenuScene.action('manage_employees', (ctx) => ctx.scene.enter('manage_employees_scene'));
 adminMenuScene.action('add_schedule', (ctx) => ctx.scene.enter('add_schedule_wizard'));
 adminMenuScene.action('view_appointments', (ctx) => ctx.scene.enter('view_appointments_scene'));
+adminMenuScene.action('edit_about_info', async (ctx) => {
+  await ctx.answerCbQuery();
+  return ctx.scene.enter('edit_info_wizard');
+});
 adminMenuScene.action('exit_admin', (ctx) => {
   ctx.reply('Вы вышли из панели управления.');
   return ctx.scene.leave();
